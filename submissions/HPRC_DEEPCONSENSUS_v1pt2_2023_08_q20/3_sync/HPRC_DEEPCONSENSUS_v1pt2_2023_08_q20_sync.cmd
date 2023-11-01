29b58e4d30e8df51f9f25a68f481969c02880255
iviolich <131207682+iviolich@users.noreply.github.com>
2023-11-01 12:31:06 -0700

    wrangling logs for dc second batch

---
+++ submissions/HPRC_DEEPCONSENSUS_v1pt2_2023_08_q20/3_sync/HPRC_DEEPCONSENSUS_v1pt2_2023_08_q20_sync.cmd
@@ -0,0 +1,5 @@
+ssds staging sync \
+    --submission-id 42AFCE59-2972-45EE-BB72-5B84E7B57377 \
+    --deployment gcp \
+    --dst-deployment default \
+    &> HPRC_DEEPCONSENSUS_v1pt2_2023_08_q20_sync.log &
