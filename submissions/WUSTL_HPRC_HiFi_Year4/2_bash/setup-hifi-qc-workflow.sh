606361010ab276ed6a56757c10897734d1b2d8c0
Andrew Blair <apblair@ucsc.edu>
2024-05-15 15:54:27 -0700

    Delete submissions/WUSTL_HPRC_HiFi_Year4 directory
    rm from main. working in branch data-wrangling

--- submissions/WUSTL_HPRC_HiFi_Year4/2_bash/setup-hifi-qc-workflow.sh
+++
@@ -1,7 +0,0 @@
-BUCKET_PATH="s3://human-pangenomics/submissions/72fad738-62d9-42a9-ba8c-53e355eb442b--WUSTL_HPRC_HiFi_Y4/"
-
-# Get the list of all .bam files from the specified S3 bucket
-FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | grep '\.hifi_reads\..*\.bam$' | awk '{print "s3://human-pangenomics/" $5}')
-
-# Extract only the first identifier from each file path
-echo "$FILES" > WUSTL_HPRC_HiFi_Y4.csv
