2d25eb053586f42ba8f80090173f25134620cb6b
Andrew Blair <apblair@ucsc.edu>
2024-05-15 15:53:00 -0700

    Delete submissions/RU_Y4 directory
    rm from main. working in branch data-wrangling

--- submissions/RU_Y4/2_bash/setup-hifi-qc-workflow.sh
+++
@@ -1,7 +0,0 @@
-BUCKET_PATH="s3://human-pangenomics/submissions/5A0D539A-4AD5-4FBF-A0FB-2DDCE5B0FA33--RU_Y4/"
-
-# Get the list of all .bam files from the specified S3 bucket
-FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | grep '\.hifi_reads\..*\.bam$' | awk '{print "s3://human-pangenomics/" $5}')
-
-# Extract only the first identifier from each file path
-echo "$FILES" > RU_Y4.csv
