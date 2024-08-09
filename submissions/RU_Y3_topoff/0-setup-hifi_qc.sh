cf015c7b8e6ba1c158fe1f0af39e55dc5da12dd6
Andrew Blair <apblair@ucsc.edu>
2024-08-08 21:02:17 -0700

    Delete submissions/RU_Y3_topoff directory

--- submissions/RU_Y3_topoff/0-setup-hifi_qc.sh
+++
@@ -1,8 +0,0 @@
-### Pull S3 Paths ###
-BUCKET_PATH="s3://human-pangenomics/submissions/573E04D5-CDDE-43B8-94D9-D00DD7B471AB--RU_Y3_topoff/"
-
-# Get the list of all .bam files from the specified S3 bucket, excluding .bam.pbi files
-FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | grep '\.bam$' | grep -v '\.bam\.pbi$' | awk '{print "s3://human-pangenomics/" $5}')
-
-# Extract only the first identifier from each file path
-echo "$FILES" > RU_Y3_topoff.csv 
--- submissions/RU_Y3_topoff/6-ssds-working.sh
+++
@@ -1,7 +0,0 @@
-# Release from submission to working
-ssds staging release \
-    --deployment default \
-    --submission-id 573E04D5-CDDE-43B8-94D9-D00DD7B471AB \
-    --transfer-csv RU_Y3_topoff.transfer.csv \
-    &> RU_Y3_topoff.transfer.log
-
