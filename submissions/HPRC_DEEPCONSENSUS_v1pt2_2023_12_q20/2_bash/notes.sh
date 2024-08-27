22eae0754ee3502df62f7a2813100cc12b5390c1
iviolich <131207682+iviolich@users.noreply.github.com>
2024-08-27 12:15:04 -0700

    Update notes.sh to include release.csv creation

--- submissions/HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20/2_bash/notes.sh
+++ submissions/HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20/2_bash/notes.sh
@@ -57,3 +57,34 @@ find . -name *summary.tsv | head -n 1 | xargs -I {} head -n 1 {} >> HPRC_DEEPCON
 for i in $(find . -name *summary.tsv)
     do tail -n 1 $i >> HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20_summary_agg.tsv
 done
+
+#create release csv, keeping in mind this is a mix of HPRC and HPRC_PLUS samples
+
+#list out sample ids in HPRC_PLUS and HPRC
+aws s3 ls s3://human-pangenomics/working/HPRC_PLUS/ | sed 's/^.*PRE //g' | sed 's/\/$//g' | sort -u > HPRC_PLUS_id.list
+aws s3 ls s3://human-pangenomics/working/HPRC/ | sed 's/^.*PRE //g' | sed 's/\/$//g' | sort -u > HPRC_id.list
+
+#check number of rows in input
+wc -l s3_submission_fastq.list
+
+#subset s3_submission_fastq.list to HPRC_PLUS and HPRC samples
+for i in $(cat ../HPRC_PLUS_id.list); do grep $i s3_submission_fastq.list >> s3_submission_fastq_hprc_plus.list; done
+for i in $(cat ../HPRC_id.list); do grep $i s3_submission_fastq.list >> s3_submission_fastq_hprc.list; done
+
+#check that total number of rows in subset files matches input
+wc -l s3_submission_fastq_hprc_plus.list s3_submission_fastq_hprc.list
+
+#replace s3 submission paths with working paths
+sed 's/submissions\/3A25CF8A-1F36-42EE-BC9F-D29CECAA2A99--HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20/working\/HPRC/g' s3_submission_fastq_hprc.list > s3_submission_fastq_hprc_working_destination.list
+sed 's/submissions\/3A25CF8A-1F36-42EE-BC9F-D29CECAA2A99--HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20/working\/HPRC_PLUS/g' s3_submission_fastq_hprc_plus.list > s3_submission_fastq_hprc_plus_working_destination.list
+
+#check if working paths don't exist
+for i in $(cat s3_submission_fastq_hprc_working_destination.list); do echo $i; aws s3 ls $i; done
+for i in $(cat s3_submission_fastq_hprc_plus_working_destination.list); do echo $i; aws s3 ls $i; done
+
+#paste submission and working paths together
+paste s3_submission_fastq_hprc.list s3_submission_fastq_hprc_working_destination.list -d, > HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20_release.csv
+paste s3_submission_fastq_hprc_plus.list s3_submission_fastq_hprc_plus_working_destination.list -d, >> HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20_release.csv
+
+#confirm number of rows matches input
+wc -l HPRC_DEEPCONSENSUS_v1pt2_2023_12_q20_release.csv
