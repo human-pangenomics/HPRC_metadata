64c8b741b540236fe2230883296d3f5d45b695fb
apblair <apblair@ucsc.edu>
2024-09-03 15:18:44 -0700

    add WUSTL_HPRC_HiFi_Year2_TopUp

---
+++ submissions/WUSTL_HPRC_HiFi_Year2_TopUp/6-ssds-working.sh
@@ -0,0 +1,6 @@
+# Release from submission to working
+ssds staging release \
+    --deployment default \
+    --submission-id 67b71022-da25-40ef-b56f-52297e946b32 \
+    --transfer-csv WUSTL_HPRC_HiFi_Year2_TopUp.transfer.csv \
+    &> WUSTL_HPRC_HiFi_Year2_TopUp.transfer.log
