9867742f4aa9f4e4ec22cdb0a357643d66e0b762
Andrew Blair <a3blair5@gmail.com>
2024-09-30 20:48:03 -0700

    Add Illumina 1kg aws submission data tables

--- submissions/Illumina_1KG/4-wrangle-illumina.sh
+++ submissions/Illumina_1KG/4-wrangle-illumina.sh
@@ -5,7 +5,7 @@ BUCKET_PATH="s3://human-pangenomics/submissions/325b4b1c-9f20-49be-b03a-596da89c
 FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | awk '{print "s3://human-pangenomics/" $5}')
 
 # Extract only the first identifier from each file path
-echo "$FILES" > Illumina_Y1-Y4.csv # NOTE: rerunning this with 5mc and primrose will cause this to fail
+echo "$FILES" > CHILDREN_1000G_DATA.csv # NOTE: rerunning this with 5mc and primrose will cause this to fail
 
 
 BUCKET_PATH="s3://human-pangenomics/submissions/59C50DDF-5FAF-4841-AC3E-6C02D636C57F--Y4_1000G_DATA/"
@@ -13,3 +13,23 @@ BUCKET_PATH="s3://human-pangenomics/submissions/59C50DDF-5FAF-4841-AC3E-6C02D636
 
 FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | awk '{print "s3://human-pangenomics/" $5}')
 echo "$FILES" > Y4_1000G_DATA.csv
+
+
+### Pull S3 Paths ###
+BUCKET_PATH="s3://human-pangenomics/submissions/AD30A684-C7A8-4D24-89B2-040DFF021B0C--Y2_1000G_DATA/"
+
+# Get the list of all .bam files from the specified S3 bucket, excluding .bam.pbi files
+FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | awk '{print "s3://human-pangenomics/" $5}')
+
+# Extract only the first identifier from each file path
+echo "$FILES" > Y2_1000G_DATA.csv # NOTE: rerunning this with 5mc and primrose will cause this to fail
+
+
+### Pull S3 Paths ###
+BUCKET_PATH="s3://human-pangenomics/submissions/54D542ED-6650-4A12-8B40-0F92CC320486--1000G_parents/"
+
+# Get the list of all .bam files from the specified S3 bucket, excluding .bam.pbi files
+FILES=$(aws s3 ls $BUCKET_PATH --recursive --no-sign-request --human-readable --summarize | awk '{print "s3://human-pangenomics/" $5}')
+
+# Extract only the first identifier from each file path
+echo "$FILES" > PARENTS_1000G_DATA.csv # NOTE: rerunning this with 5mc and primrose will cause this to fail
