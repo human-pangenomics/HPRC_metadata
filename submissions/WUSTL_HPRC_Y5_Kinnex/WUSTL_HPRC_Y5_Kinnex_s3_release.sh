4b0a69161e5a91f96535f1b3f52d6fbe7355e6df
Andrew Blair <a3blair5@gmail.com>
2025-01-26 18:09:24 -0800

    add s3 release

---
+++ submissions/WUSTL_HPRC_Y5_Kinnex/WUSTL_HPRC_Y5_Kinnex_s3_release.sh
@@ -0,0 +1,5 @@
+ssds staging release \
+    --deployment default \
+    --submission-id d22744ed-213c-43b3-a91d-31a282bb6e56 \
+    --transfer-csv WUSTL_HPRC_Y5_Kinnex_s3_working.csv \
+    &> WUSTL_HPRC_Y5_Kinnex.transfer.log
