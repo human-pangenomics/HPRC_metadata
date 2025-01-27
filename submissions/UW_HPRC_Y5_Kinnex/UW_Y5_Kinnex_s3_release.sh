3ae89b7b399d73676f12800c486143d8a249f1e2
Andrew Blair <a3blair5@gmail.com>
2025-01-26 18:09:44 -0800

    add s3 release

---
+++ submissions/UW_HPRC_Y5_Kinnex/UW_Y5_Kinnex_s3_release.sh
@@ -0,0 +1,5 @@
+ssds staging release \
+    --deployment default \
+    --submission-id 92fa20c5-5bdb-41ca-bd79-e6ccead281b3 \
+    --transfer-csv UW_Y5_Kinnex_s3_working.csv \
+    &> UW_Y5_Kinnex.transfer.log
