26aa842833b985730bec2bd7348c6cbb30eaabcd
Andrew Blair <a3blair5@gmail.com>
2025-01-26 18:10:04 -0800

    add s3 release

---
+++ submissions/RU_Y5_Kinnex/RU_Y5_Kinnex_s3_release.sh
@@ -0,0 +1,5 @@
+ssds staging release \
+    --deployment default \
+    --submission-id FCD6C271-5641-4C1C-9B2D-C6E2BF419E57 \
+    --transfer-csv RU_Y5_Kinnex_s3_working.csv \
+    &> RU_Y5_Kinnex.transfer.log
