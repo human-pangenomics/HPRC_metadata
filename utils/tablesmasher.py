e3c18b8d050767ebb10b6067ee077371f97f1f6c
Ash O'Farrell <aofarrel@ucsc.edu>
2025-08-12 16:11:59 -0700

    Now with actually working code!

--- utils/tablesmasher.py
+++ utils/tablesmasher.py
@@ -1,74 +1,84 @@
-# This !unfinished! script has the rather ambitious goal of merging our HPRC index tables (the ones from Google Sheets)
-# with information from this repo, most notably SRR IDs but also any corrections... without just overriding
-# every difference we find, since what's in this repo might actually be less correct!
+# This script is used to merge the following files:
+# 1) index_sheets: Any arbitrary number of TSVs from Google Sheets, each with a `filename` column
+#    ---> In our case, that is a bunch of "WORKING R2 Sequencing Data Index" sheets (dc, hic, hifi, kinndex, ont, ill)
+#    ---> All values for `filename` must be unique across all `index_sheets` (script will throw err if dupes detected)
+# (This is a Python dictionary where the key is a shorthand unique name for a sheet and the value is its path)
+index_sheets = {
+"dc": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - dc.tsv",
+"hic": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - hic.tsv",
+"hifi": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - hifi.tsv",
+"kinnex": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - kinnex.tsv",
+"ont": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - ont.tsv",
+"ill": "/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-11_index_files/WORKING R2 Sequencing Data Index - ill.tsv"
+}
+# 2) manifest_file: One concatenated manifest CSV, which also has a `filename` column
+#    ---> In our case, that is a concatenation of summary information I gathered from the AnVIL file transfers
+manifest_file = "/Users/aofarrel/github/HPRC_metadata/utils/AnVIL_transfer/logs_and_manifests/manifests_2025-07-14.csv"
+# 3) wrangled_sheets: Any arbitrary number of CSVs with other metadata, each with a `filename` column
+#    ---> In our case, these are the `__final.csv` files I made with corrected metadata and SRA accessions
+#    ---> All values for `filename` must be unique across all `wrangled_sheets` (script will throw err if dupes detected)
+wrangled_sheets = [
+"/Users/aofarrel/github/HPRC_metadata/submissions/HPRC_DEEPCONSENSUS_v1pt2/HPRC_DEEPCONSENSUS_v1pt2_data_table__final.csv",
+"/Users/aofarrel/github/HPRC_metadata/submissions/RU_Y2_topoff/RU_Y2_topoff_data_table__final.csv",
+"/Users/aofarrel/github/HPRC_metadata/submissions/UW_HPRC_HiFi_Y1/UW_HPRC_HiFi_Y1_data_table__final.csv"
+]
 #
-# To that end, this script will report differences where it finds them. As such you'll probably need to
-# run it multiple times, adding to the following lists that will be used to determine which data
-# is correct. Although designed for R2 data, this will work any arbitrary dataset.
+# The output will be a single TSV with merged information from all three of these sources, using these rules:
+# a) All files were be merged upon the `filename` column (at runtime, renamed to `__index__filename`)
+# b) Any columns that are in both `manifest` and any `index_sheet` will be ignored -- that is to say, it will not be
+# checked, it will not be overwritten, it will just remain whatever it was in `index_sheets`. 
+# c) Any non-`filename` columns shared between any member of `index_sheets` and any member of `wrangled_sheets` will
+# be nullfilled. If there are actual data conflicts, the script will throw an error and tell you what the conflicts are,
+# unless the conflict is in `overrides`...
+overrides = ['filetype']
+# ...or you set `wrangled_vital_columns` to a non-empty value, in which case, only THOSE columns are considered
+# for conflicts. This is for people who do not actually want to validate data and just want to merge stuff quickly.
+wrangled_vital_columns = ['accession', 'sample_ID'] # don't include `filename` here!
+wrangled_vital_columns.append('__index__filename')  # don't touch this line!!
 #
-# First of all: You'll need my library Ranchero. Because I'm still playing with its details, Ranchero is
+# One last thing: You'll need my library Ranchero. Because I'm still playing with its details, Ranchero is
 # currently not pip-installable. You will need to `git clone` the entire repo from my (aofarrel) GitHub,
 # `pip install -r requirements.txt`, then add Ranchero's path here:
 ranchero_path = '/Users/aofarrel/github/Ranchero'
 import sys
 sys.path.insert(0, ranchero_path) 
+assert sys.version_info >= (3, 7), f"Use Python 3.7 or newer -- you are using {sys.version_info[0]}.{sys.version_info[1]}"
 import src as Ranchero
-#
-# Now, put the path of your sheets from the Google Sheets/Excel. They must be in TSV format. Since there's
-# likely several, we're gonna hardcode them rather than have you pass a bunch of args. All of these files must
-# have a "filename" column, and there can be no duplicates in the "filename" column across any index_sheets.
-# They will be combined into one big index called main_index.
-index_sheets = [
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - dc.tsv",
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - hic.tsv",
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - hifi.tsv",
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - kinnex.tsv",
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-07_index_files/WORKING R2 Sequencing Data Index - ont.tsv",
-"/Users/aofarrel/github/HPRC_metadata/utils/2025-Aug-11_index_files/WORKING R2 Sequencing Data Index - ill.tsv"
-]
-#
-# This file is also keyed by filename. It has columns that are not in index_sheets that should be added to index_sheets.
-# Any columns that are in both `manifest` and any `index_sheet` will be ignored -- that is to say, it will not be
-# checked, it will not be overwritten, it will just remain whatever it was in `index_sheets`. Also, it's expected
-# to be a CSV file, not a TSV file!
-manifest_file = "/Users/aofarrel/github/HPRC_metadata/utils/AnVIL_transfer/logs_and_manifests/manifests_2025-07-14.csv"
-#
-# Here are the sheets we're combining with. These are currently hardcoded with the `__final.csv` files
-# from this repo but you can put whatever here. Doesn't matter if they're split by sequencing tech or
-# collection or submission ID or whatever; we're smashing everything into one megatable. Even if this
-# means some rows aren't relevant to some samples.
-wrangled_sheets = [
-"/Users/aofarrel/github/HPRC_metadata/submissions/HPRC_DEEPCONSENSUS_v1pt2/HPRC_DEEPCONSENSUS_v1pt2_data_table__final.csv",
-"/Users/aofarrel/github/HPRC_metadata/submissions/RU_Y2_topoff/RU_Y2_topoff_data_table__final.csv",
-"/Users/aofarrel/github/HPRC_metadata/submissions/UW_HPRC_HiFi_Y1/UW_HPRC_HiFi_Y1_data_table__final.csv",
-]
-
-# Columns that you want to always have the main_index override the wrangled sheet
-overrides = ['filetype', 'instrument_model', 'design_description', 'library_ID', 'size_selection', 'polymerase_version', 'seq_plate_chemistry_version', 'ccs_algorithm', 'notes']
-
-
 
 # TODO: allow user to select if they want left or right for conflicts
+# NOTE: You can use polars expressions to validate `manifest_s3_source` against `path` but you need to handle nulls properly
 
 
 import os
 import polars as pl
 import polars.selectors as cs
 
-main_index = Ranchero.from_tsv(index_sheets[0], index='filename', auto_standardize=False, auto_rancheroize=False)
+Ranchero.Configuration.set_config({"dupe_index_handling": 'verbose_error'})
+zeroth_index_sheet = next(iter(index_sheets.items()))
+main_index = Ranchero.from_tsv(zeroth_index_sheet[1], index='filename', auto_standardize=False, auto_rancheroize=False)
+main_index = main_index.with_columns(pl.lit(zeroth_index_sheet[0]).alias("index_sheet"))
 main_index = main_index.with_columns((~cs.string()).cast(pl.String)) # forces all columns into type str
-print(f"Processed {os.path.basename(index_sheets[0])} into a {main_index.shape} dataframe with columns {main_index.columns}")
+print(f"Processed {os.path.basename(zeroth_index_sheet[0])} into a {main_index.shape} dataframe")
 if len(index_sheets) > 1:
-	for sheet_path in index_sheets[1:]:
+	for sheet_name, sheet_path in list(index_sheets.items())[1:]:
 		another_index = Ranchero.from_tsv(sheet_path, index='filename', auto_standardize=False, auto_rancheroize=False)
-		print(f"Processed {os.path.basename(sheet_path)} into a {another_index.shape} dataframe with columns {another_index.columns}")
+		another_index = another_index.with_columns(pl.lit(sheet_name).alias("index_sheet"))
+		print(f"Processed {sheet_name} into a {another_index.shape} dataframe")
 		another_index = another_index.with_columns((~cs.string()).cast(pl.String)) # forces all columns into type str
 		main_index = pl.concat([main_index, another_index], how='align_full')
 		main_index = Ranchero.NeighLib.check_index(main_index, df_name='main')
-print(f"Combined all index_sheets into a {main_index.shape} dataframe:")
-print(main_index)
+print(f"Combined all index_sheets into a {main_index.shape} dataframe.")
+if 'path' in main_index.columns:
+	main_index = main_index.with_columns(
+		pl.when(pl.col('path').str.contains("s3://human-pangenomics/working"))
+		.then(True)
+		.otherwise(False)
+		.alias('in_working'))
+	in_working = main_index.filter(pl.col('in_working'))
+	print(f"Out of {main_index.shape[0]} files, {in_working.shape[0]} are in working")
 
 if manifest_file is not None:
+	Ranchero.Configuration.set_config({"dupe_index_handling": 'keep_most_data'})
 	manifest = Ranchero.from_tsv(manifest_file, delimiter=",", index='filename', auto_standardize=False, auto_rancheroize=False)
 	print(f"Processed manifest file into a {manifest.shape} dataframe")
 	manifest = Ranchero.NeighLib.check_index(manifest, df_name='manifest')
@@ -76,12 +86,20 @@ if manifest_file is not None:
 		left_name='main', right_name='manifest', 
 		fallback_on_left=True) # any disrepencies (which should not happen) will fall back to the main_index
 	print(f"Merged main_index with manifest to create an index of shape {main_index.shape}")
-	Ranchero.dfprint(main_index.select(['__index__filename', 'accession', 'path', 'manifest_s3_source', 'manifest_gs_path']))
+	Ranchero.dfprint(main_index.select(['__index__filename', 'accession', 'path', 'manifest_checksum', 'manifest_gs_path']))
+	print("Rows without values for manifest_checksum:")
+	Ranchero.dfprint(
+		main_index.filter(pl.col('manifest_gs_path').is_null())
+		.select(Ranchero.NeighLib.valid_cols(main_index, ['__index__filename', 'path', 'index_sheet'])), str_len=200
+	)
 
+Ranchero.Configuration.set_config({"dupe_index_handling": 'verbose_error'})
 for wrangled_path in wrangled_sheets:
 	print(f"Processing {wrangled_path}...")
 	this_wrangled_name = os.path.basename(wrangled_path)
 	this_wrangled = Ranchero.from_tsv(wrangled_path, delimiter=",", index='filename', auto_standardize=False, auto_rancheroize=False)
+	if len(wrangled_vital_columns) != 0:
+		this_wrangled = this_wrangled.select(wrangled_vital_columns)
 	this_wrangled = this_wrangled.with_columns((~cs.string()).cast(pl.String)) # forces all columns into type str
 	Ranchero.kolumns.list_fallback_or_null = overrides
 	Ranchero.kolumns.list_throw_error = [x for x in this_wrangled.columns if x not in overrides]
@@ -89,7 +107,9 @@ for wrangled_path in wrangled_sheets:
 		left_name='main', right_name=this_wrangled_name, 
 		fallback_on_left=False) # fallbacks (if allowed per kolumns) will fallback on right
 	print(f"Merged with {this_wrangled_name}")
-	print(main_index)
 
-
+print("Finished.")
+Ranchero.dfprint(
+	main_index.select(Ranchero.NeighLib.valid_cols(main_index, 
+		['__index__filename', 'accession', 'path', 'manifest_checksum', 'manifest_gs_path', "index_sheet", "in_working"])))
 
